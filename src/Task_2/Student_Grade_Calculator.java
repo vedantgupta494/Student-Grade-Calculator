1978e0869ee3d64da5700af3618091ae20e6c925
Vedant Gupta <vedantgupta494@gmail.com>
2025-05-22 11:47:29 +0530

    Update Student_Grade_Calculator.java

--- src/Task_2/Student_Grade_Calculator.java
+++ src/Task_2/Student_Grade_Calculator.java
@@ -2,7 +2,7 @@ package Task_2;
 import java.util.Scanner;
 public class Student_Grade_Calculator {
     public static void main(String[] args) {
-        Scanner sc=new Scanner (System.in);
+        Scanner sc= new Scanner (System.in);
         System.out.print("English: ");
         double a = sc.nextDouble();
         System.out.print("Math: ");
